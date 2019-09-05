9db99f8a1221bbac187f655c1ccd44f645de7d1a
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-05 19:00:58 +0500

    make it better

--- flask_app.py
+++ flask_app.py
@@ -22,16 +22,17 @@ def  LR():
     return render_template('LR.html')
 
     
-@flask_app.route('/LR_prediction', methods=['post'])
+@flask_app.route('/LR_prediction', methods=['POST'])
 def  LR_prediction():
+    if request.method == 'POST':
     
-    initial_features = [float(x)/10000  for x in request.form.values()]
-    features = np.array(initial_features)
-    f_features = np.hstack((1,features))
-    f_features = np.array(f_features)
-    pred = lRegression(f_features)
-    string = 'The value of profit in $10000 is ' + str(pred)
-    return render_template('LR.html', prediction_text = string)
+        initial_features = [float(x)  for x in request.form.values()]
+        features = np.array(initial_features)
+        f_features = np.hstack((1,features))
+        f_features = np.array(f_features)
+        pred = lRegression(f_features)
+        string = 'The value of profit in $10000 is ' + str(pred)
+        return render_template('LR.html', prediction_text = string)
 
 
 @flask_app.route('/MLR')
@@ -39,15 +40,17 @@ def MLR():
     return render_template('MLR.html')
 
 
-@flask_app.route('/MLR_prediction', methods=['post'])
+@flask_app.route('/MLR_prediction', methods=['POST'])
 def MLR_prediction():
-    features = [float(x) for x in request.form.values()]
-    features = np.array(features) .reshape((1,2))
-    #one = np.ones((features.shape[0], 1))
-    #features = np.hstack((one , features))
-    pred = LR_multiple(features)
-    string = 'The price of house is ' + str(pred)
-    return render_template('MLR.html', prediction_text = string)
+    if request.method == 'POST':
+            
+        features = [float(x) for x in request.form.values()]
+        features = np.array(features) .reshape((1,2))
+        #one = np.ones((features.shape[0], 1))
+        #features = np.hstack((one , features))
+        pred = LR_multiple(features)
+        string = 'The price of house is ' + str(pred)
+        return render_template('MLR.html', prediction_text = string)
 
                 
     
